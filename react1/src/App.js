509bfc359f4608fd934296dfec62ce3f468101df
Doommez <jenyaz862@gmail.com>
2021-11-01 16:35:32 +0300

    add test react app

---
+++ react1/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  test
+  <ul>qeqrqerq
+    <li>asdf1</li>
+    <li>asdf2</li>
+    <li>asdf3</li>
+  </ul>
+    </div>
+  );
+  }
+
+  
+export default App;
+
